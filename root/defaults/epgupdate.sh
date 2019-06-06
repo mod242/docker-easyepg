437a3e8ad3048817ed931f354b49576c5bab68e5
mod242 <40213799+mod242@users.noreply.github.com>
2019-06-06 09:18:56 +0200

    Update epgupdate.sh

--- root/defaults/epgupdate.sh
+++ root/defaults/epgupdate.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+chown -R abc:users /easyepg
+chown -R abc:users /tmp
+
 cd /easyepg || exit
 
-/bin/bash /easyepg/epg.sh
+s6-setuidgid abc /bin/bash /easyepg/epg.sh
+
+chown -R abc:users /easyepg
+chown -R abc:users /tmp
 
 exit 0
